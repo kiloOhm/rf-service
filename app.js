80104b9ce64affe126f40dc6fc18a5a1275cc6a4
Ohm <49402476+kiloOhm@users.noreply.github.com>
2022-06-18 17:14:23 +0200

    access control

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const servers = {
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", req.headers['origin'] ?? "*");
-  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH, PUT");
+  res.header("Access-Control-Allow-Methods", "GET");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
